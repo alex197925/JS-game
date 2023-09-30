9e4c984c70663219f01d977c1d179c452d42236a
Tolok Oleksandr <tolokoleksandr@Toloks-MacBook-Pro.local>
2023-09-30 13:04:37 +0200

    Generate random number, Set path of the images, Set to attribute src dynamic value

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 /** @format */
+// Generate random number from 1 to 6.9999999999
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-let randomNumber1 = Math.floor((Math.random()* 6) + 1);
 
-console.log(randomNumber1);
+// Select path of the images and combine together with randomNumber
+randomImages1 = "images/dice" + randomNumber1 +  ".png";
+console.log(randomImages1);
+
+
+
+// Set to attribute src dynamic value of "randomImages1"
+document.querySelector(".dice .img1").setAttribute("src", randomImages1);
+
+
