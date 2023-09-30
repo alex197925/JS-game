53fdd28568d58e453c6761db4b9410f0739a6970
Tolok Oleksandr <tolokoleksandr@Toloks-MacBook-Pro.local>
2023-09-30 13:09:01 +0200

    Add random secont image

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 /** @format */
 // Generate random number from 1 to 6.9999999999
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 // Select path of the images and combine together with randomNumber
 randomImages1 = "images/dice" + randomNumber1 +  ".png";
+randomImages2 = "images/dice" + randomNumber2 +  ".png";
 console.log(randomImages1);
 
 
 
 // Set to attribute src dynamic value of "randomImages1"
 document.querySelector(".dice .img1").setAttribute("src", randomImages1);
-
+document.querySelector(".dice .img2").setAttribute("src", randomImages2);
 
